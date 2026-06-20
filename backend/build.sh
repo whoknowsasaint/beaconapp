b628d2d629d769c0ac0a1381b1e254ff298894cd
unknown <hidarc32@gmail.com>
2026-06-19 22:37:23 -0400

    build

--- backend/build.sh
+++ backend/build.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -o errexit
 
+cd backend
 pip install -r requirements.txt
 python manage.py collectstatic --no-input --settings=core.settings.production
 python manage.py migrate --settings=core.settings.production
